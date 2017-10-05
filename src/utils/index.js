b2abb3eb0df7f9cc4553194dde71ac21df5eb6da
Ross Leitch <Ross@metamorphicsolutions.com>
2017-10-05 14:32:01 +1300

    Removed context from flatten util

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,7 +7,7 @@ var isArray = (array) => {
 
 var flatten = (arr) => {
    return arr.reduce((flat, toFlatten) => {
-      return flat.concat(Array.isArray(toFlatten) ? this.flatten(toFlatten) : toFlatten);
+      return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
    }, []);
 }
 
