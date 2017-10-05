4ea1c82f82e5f20ef916e423e7ecdc0d7494d29d
Ross Leitch <Ross@metamorphicsolutions.com>
2017-10-05 15:07:45 +1300

    Updated prebuilts

--- dist-es6/utils/index.js
+++ dist-es6/utils/index.js
@@ -1,5 +1,3 @@
-var _this = this;
-
 var isArray = function isArray(array) {
    for (var i = 0; i < array.length; i++) {
       if (Array.isArray(array[i])) return true;
@@ -9,7 +7,7 @@ var isArray = function isArray(array) {
 
 var flatten = function flatten(arr) {
    return arr.reduce(function (flat, toFlatten) {
-      return flat.concat(Array.isArray(toFlatten) ? _this.flatten(toFlatten) : toFlatten);
+      return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
    }, []);
 };
 
